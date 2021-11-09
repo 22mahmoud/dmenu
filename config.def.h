3bcb9f32005c8ca696ef0c934f51ffa2af38a499
22mahmoud <hello@mahmoudashraf.dev>
2021-11-09 22:53:19 +0200

    fix pango patch

--- config.def.h
+++ config.def.h
@@ -3,9 +3,8 @@
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
-static const char *fonts[] = {
-	"monospace:size=10"
-};
+static const char font[] = "monospace 10";
+
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
