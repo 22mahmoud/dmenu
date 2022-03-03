69ad95159191d4855e5f936111579b7a5ca65118
22mahmoud <hello@mahmoudashraf.dev>
2022-03-03 19:16:18 +0200

    fix warnnings

--- config.def.h
+++ config.def.h
@@ -4,7 +4,7 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
-static const char font[] = "monospace 10";
+static char font[] = "monospace 10";
 
 static char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 
